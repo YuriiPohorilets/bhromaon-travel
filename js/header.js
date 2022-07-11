81f2afe2feeb9a11c379abb5d64cb600e11c2356
YuriiPohorilets <pohorilets.y@gmail.com>
2022-07-11 22:00:33 +0300

    add modal

--- js/header.js
+++ js/header.js
@@ -1,8 +1,10 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var header = document.getElementById('myHeader');
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
@@ -10,8 +12,8 @@ var sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
+    header.classList.add('sticky');
   } else {
-    header.classList.remove("sticky");
+    header.classList.remove('sticky');
   }
 }
